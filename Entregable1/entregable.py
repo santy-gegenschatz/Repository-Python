bae8b13cf11e7a88532c792fae95f65664fbb841
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2023-03-08 14:23:50 -0300

    [bug fix]

--- Entregable1/entregable.py
+++ Entregable1/entregable.py
@@ -1,3 +1,5 @@
+import time
+
 class User():
     def __init__(self, username, password):
         self.username = username
@@ -6,12 +8,12 @@ class User():
         return f'{self.username}, {self.password}'
 
 class Server():
-    def __init__(self, usuarios):
-        self.usuarios = usuarios
+    def __init__(self, users):
+        self.users = users or []
 
     def login(self, username, password):
         try:
-            for user in self.usuarios:
+            for user in self.users:
                 if (user.username == username):
                     if (user.password == password):
                         return f'Welcome to the server {username}'
@@ -24,25 +26,44 @@ class Server():
     
     def signup(self, username, password):
         try:
-            for user in self.usuarios:
+            for user in self.users:
                 if (username == user.username):
                     return f'The username {username} is already taken'
             new_user = User(username, password)
-            self.usuarios.append(new_user)
+            self.users.append(new_user)
             return f'The user {username} has been successfully created'
         except:
             # In this case this is the first user to be created
-            self.usuarios = []
             new_user = User(username, password)
-            self.usuarios.append(new_user)
+            self.users.append(new_user)
             return f'The user {username} has been successfully created'
 
     def save_data(self, database_name):
         with open(f'{database_name}.txt', 'w') as file:
-            for item in self.usuarios:
-                file.write(f"{item}\n")
+            file.write('Users\n')
+            if self.users is not None:
+                for item in self.users:
+                    file.write(f"{item}\n")
+
+
+    def read_data(self, database_name):
+        print('Loading Database')
+        try:
+            with open(f'{database_name}.txt', 'r') as file:
+                self.users = []
+                for index, line in enumerate(file.readlines()):
+                    if (index is not 0):
+                        username = line.split()[0].split(',')[0]
+                        password = line.split()[1]
+                        new_user = User(username, password)
+                        print(f'Loading user {index}')
+                        self.users.append(new_user)
+        except:
+            print('Database not found')
+
 
     def start(self):
+        self.read_data('database')
         run = True
         while run:
             action = input('Press L to login, S to signup, E to exit ')
@@ -57,6 +78,7 @@ class Server():
                 print('Exiting program')
                 print('Saving data to text File')
                 self.save_data('database')
+                time.sleep(1)
                 run = False
             else:
                 print('Command not recognized, please try again')
